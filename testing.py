27b1b7e624a11baa2755bfb5048a15f445d1bdba
Vagyasri <vagyasri2011@gmail.com>
2021-11-16 09:25:19 +0100

    Display String

--- testing.py
+++ testing.py
@@ -3,3 +3,7 @@
 x = int(input("Enter an integer: "))
 y = int(input("Enter another integer: "))
 print(x+y)
+
+x = str(input("Enter an string: "))
+y = str(input("Enter another string: "))
+print(x+ " " +y)
