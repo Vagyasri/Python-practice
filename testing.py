0f800fa943b7cb4229999a8518b2004a0c4b30fc
Vagyasri <vagyasri2011@gmail.com>
2021-11-16 09:45:42 +0100

    Add Comments

--- testing.py
+++ testing.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+# Interger
 x = int(input("Enter an integer: "))
 y = int(input("Enter another integer: "))
 print(x+y)
 
+# String
 x = str(input("Enter an string: "))
 y = str(input("Enter another string: "))
-print(x+ " " +y)
+print(x + " " + y)
